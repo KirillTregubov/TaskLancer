b56b65e5b70d9c2e3818a498729a6b7bfe003301
Kirill Tregubov <contact@kirilltregubov.com>
2025-01-14 20:29:47 -0500

    Setup tailwind, eslint, prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+export default {
+  arrowParens: 'always',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  plugins: ['prettier-plugin-tailwindcss']
+  // "prettier-plugin-organize-imports",
+  //   tailwindFunctions: ["clsx"],
+}
